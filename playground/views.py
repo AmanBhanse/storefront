62780b788562e2b258ea1a64210951e84a8fee6c
AmanBhanse <amanbhanse22@gmail.com>
2025-05-31 22:12:35 +0200

    Use of Template

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('hello world')
+    return render(request, 'hello.html', {'name' : 'Aman'})
